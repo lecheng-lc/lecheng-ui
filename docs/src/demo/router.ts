8d3b1fd9aa3f89a7661f691ca7b0adb0c8a95d03
lecheng <jie.yan@ingeek.com>
2022-08-08 16:22:27 +0800

    feat: 使用vite配置项目

--- docs/src/demo/router.ts
+++ docs/src/demo/router.ts
@@ -1,12 +1,15 @@
 
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import { routerDir } from '../constant'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import {defineAsyncComponent} from 'vue'
 import Home from './Home/Index'
+const modules = (import.meta as any).glob('../../../packages/**/**.tsx');
 const routes: RouteRecordRaw[] = []
 const composeRoute = (name: string): RouteRecordRaw => ({
 	path: `/${name}`,
 	name: `/${name}`,
-	component: () => import(`@@/${name}/demo/index`)
+	component: modules[`../../../packages/${name}/demo/index.tsx`]
+	// component: () => defineAsyncComponent(()=>import( /* @vite-ignore */ `@@/${name}/demo/index.tsx`))
 })
 routerDir.forEach(x => {
   x.items.forEach(item => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,112 @@
 import { defineConfig } from 'vite'
+import vitePluginVue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import path from 'path'
+import vitePluginMd from 'vite-plugin-md';
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// import dynamicImportVars from '@rollup/plugin-dynamic-import-vars'
+import hljs from 'highlight.js';
+const  createRequire  = require('module')
+// import type MarkdownIt from 'markdown-it'
 
+// add target="_blank" to all links
+function markdownLinkOpen(md: any) {
+  const defaultRender = md.renderer.rules.link_open;
+
+  md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+    const aIndex = tokens[idx].attrIndex('target');
+
+    if (aIndex < 0) {
+      tokens[idx].attrPush(['target', '_blank']); // add new attribute
+    }
+
+    if (defaultRender) {
+      return defaultRender(tokens, idx, options, env, self);
+    }
+
+    return self.renderToken(tokens, idx, options);
+  };
+}
+function markdownHighlight(str: string, lang: string) {
+  if (lang && hljs.getLanguage(lang)) {
+    // https://github.com/highlightjs/highlight.js/issues/2277
+    return hljs.highlight(str, { language: lang, ignoreIllegals: true }).value;
+  }
+
+  return '';
+}
+function markdownCardWrapper(htmlCode: string) {
+  const group = htmlCode
+    .replace(/<h3/g, ':::<h3')
+    .replace(/<h2/g, ':::<h2')
+    .split(':::');
+
+  return group
+    .map((fragment) => {
+      if (fragment.indexOf('<h3') !== -1) {
+        return `<div class="van-doc-card">${fragment}</div>`;
+      }
+      return fragment;
+    })
+    .join('');
+}
 export default defineConfig({
+  root:path.resolve(__dirname, './docs/src/'),
+  css:{
+    preprocessorOptions:{
+      styl: {
+        // additionalData: '~@@: /docs/packages;'
+      },
+    },
 
+  },
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'./docs'),
+      '@@': path.resolve(__dirname, './packages')
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  plugins: [
+    vitePluginVue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    // dynamicImportVars({
+    //   // options
+    // }),
+    Components({
+      dts: true,
+      dirs: ['./packages'], // 按需加载的文件夹
+      resolvers: [
+     ]
+    }),
+    vitePluginMd({
+      // wrapperClasses: 'van-doc-markdown-body',
+      // transforms: {
+      //   after: markdownCardWrapper,
+      // },
+      // markdownItOptions: {
+      //   typographer: false, // https://markdown-it.github.io/markdown-it/#MarkdownIt
+      //   highlight: markdownHighlight,
+      // },
+      // markdownItSetup(md: any) {
+      //   const require = createRequire(import.meta.url);
+      //   console.log(import.meta.url,'=====')
+      //   const { slugify } = require('transliteration')
+      //   const markdownItAnchor = require('markdown-it-anchor');
+      //   markdownLinkOpen(md);
+      //   md.use(markdownItAnchor, {
+      //     level: 2,
+      //     // slugify,
+      //   });
+      // },
+    }),
+    vueJsx({}),
+
+  ],
+  server: {
+    host: '0.0.0.0',
+  },
 })
